871b01bc42689739ff30eddfbb7ab86b6bea8910
vytenisblask <vytenisb@yahoo.com>
2024-01-10 19:18:26 +0200

    light/dark mode added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
 import QRCodeGenerator from './QRcodeGenerator';
+import { Flex, Box, useColorMode, Switch, Image } from '@chakra-ui/react';
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>QR Code Generator</h1>
-        <QRCodeGenerator />
-      </header>
+      <Flex className="App-header" justifyContent="space-between" alignItems="center" p={4}>
+        <Box>
+          <Image src="path-to-your-logo.svg" alt="Logo" boxSize="50px" />
+        </Box>
+        <Box>
+          <label>
+            {colorMode === 'light' ? 'Dark' : 'Light'} Mode
+            <Switch onChange={toggleColorMode} isChecked={colorMode === 'dark'} />
+          </label>
+        </Box>
+      </Flex>
+      <QRCodeGenerator />
     </div>
   );
 }
