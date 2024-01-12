09f082143a97bed032d8491237354597cfc9de4c
vytenisblask <vytenisb@yahoo.com>
2024-01-12 18:34:28 +0200

    nac divider added

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,19 +1,22 @@
 // NavigationBar.js
 import React from 'react';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, Divider } from '@chakra-ui/react';
 import ColorModeSwitcher from './ColorModeSwitcher';
 import LogoIcon from './LogoIcon';
 
 const NavigationBar = () => {
   return (
-    <Flex justifyContent="space-between" alignItems="center" p={4}>
-      <Box>
-        <LogoIcon />
-      </Box>
-      <Box>
-        <ColorModeSwitcher />
-      </Box>
-    </Flex>
+    <>
+      <Flex justifyContent="space-between" alignItems="center" p={4}>
+        <Box>
+          <LogoIcon />
+        </Box>
+        <Box>
+          <ColorModeSwitcher />
+        </Box>
+      </Flex>
+      <Divider />
+    </>
   );
 };
 
